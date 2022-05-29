49335fd824c437d16a3af5759bb9909a7609abbc
pjsrcool <jpham079@ucr.edu>
2022-05-29 16:58:10 -0700

    test

--- exec.c
+++ exec.c
@@ -63,8 +63,8 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 1)
-    goto bad;
+//  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 1)
+//    goto bad;
   clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
   sp = sz;
 
